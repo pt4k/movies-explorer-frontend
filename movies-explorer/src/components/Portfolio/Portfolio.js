b5d5a654ed25dc4500026a98ab7973637bba3284
Yuriy Pishchenko <locbne@gmail.com>
2022-11-06 12:14:01 +0300

    fix: class=portfolio__item сделал единой ссылкой

--- movies-explorer/src/components/Portfolio/Portfolio.js
+++ movies-explorer/src/components/Portfolio/Portfolio.js
@@ -6,29 +6,44 @@ function Portfolio() {
     <section className='portfolio'>
       <h2 className='portfolio__title'>Портфолио</h2>
       <ul className='portfolio__list'>
-        <li
-          className='portfolio__item'
-          href='https://pt4k.github.io/signin'
-          target='_blank'
-        >
-          <p className='portfolio__paragraph'>Статичный сайт</p>{' '}
-          <img className='portfolio__img' alt='ссылка' src={link} />
+        <li className='portfolio__item'>
+          <a
+            className='portfolio__link'
+            href='https://pt4k.github.io/signin'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <p className='portfolio__paragraph'>Статичный сайт</p>{' '}
+            <img className='portfolio__img' alt='ссылка' src={link} />
+          </a>
         </li>
-        <li
-          className='portfolio__item'
-          href='https://pt4k.github.io/signin'
-          target='_blank'
-        >
-          <p className='portfolio__paragraph'>Адаптивный сайт</p>{' '}
-          <img className='portfolio__img' alt='ссылка' src={link} />
+
+        <li className='portfolio__item'>
+          <a
+            className='portfolio__link'
+            href='https://pt4k.github.io/signin'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <p className='portfolio__paragraph'>Адаптивный сайт</p>{' '}
+            <img className='portfolio__img' alt='ссылка' src={link} />
+          </a>
         </li>
+
         <li
           className='portfolio__item'
           href='https://pt4k.github.io/signin'
           target='_blank'
         >
-          <p className='portfolio__paragraph'>Одностраничное приложение</p>{' '}
-          <img className='portfolio__img' alt='ссылка' src={link} />
+          <a
+            className='portfolio__link'
+            href='https://pt4k.github.io/signin'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <p className='portfolio__paragraph'>Одностраничное приложение</p>{' '}
+            <img className='portfolio__img' alt='ссылка' src={link} />
+          </a>
         </li>
       </ul>
     </section>
